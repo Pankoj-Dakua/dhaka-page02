34bcf61b18c45d9270af49a3e0b2f4b04630fe4a
Pankoj-Dakua <pankoj6@gmail.com>
2026-03-27 21:09:11 +0600

    Magor change

---
+++ JS/javascript.js
@@ -0,0 +1,25 @@
+
+// Start jascript for navbar.css
+
+//For hide or shwo menu bar
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+
+// End jascript for navbar.css
+
+
+
+
+
+
+
+
+
+
+
